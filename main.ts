5dca17a1777aa642b0f2031d294dc979091414ad
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2024-09-07 19:42:33 +0100

    Finished

--- main.ts
+++ main.ts
@@ -1,1 +1,75 @@
-// Copy and paste your HackerRank code here
+"use strict";
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+function rotLeft(a: number[], d: number): number[] {
+  if (a.length <= 1) {
+    return a;
+  }
+
+  const copyOfA = a.slice();
+
+  let numberOfRotationsLeft = d;
+  while (numberOfRotationsLeft !== 0) {
+    const val = copyOfA.shift();
+    if (val) {
+      copyOfA.push(val);
+      numberOfRotationsLeft -= 1;
+    }
+  }
+
+  return copyOfA;
+}
+
+function main() {
+  const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]!);
+
+  const firstMultipleInput: string[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ");
+
+  const n: number = parseInt(firstMultipleInput[0], 10);
+
+  const d: number = parseInt(firstMultipleInput[1], 10);
+
+  const a: number[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
+
+  const result: number[] = rotLeft(a, d);
+
+  ws.write(result.join(" ") + "\n");
+
+  ws.end();
+}
